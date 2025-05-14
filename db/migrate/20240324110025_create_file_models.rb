53f862c574a4e4ad871b383646b6298a0ac80f37
Faizan Haider <hfaizan53@gmail.com>
2025-05-14 18:32:05 +0500

    migration updated

--- db/migrate/20240324110025_create_file_models.rb
+++ db/migrate/20240324110025_create_file_models.rb
@@ -2,6 +2,7 @@ class CreateFileModels < ActiveRecord::Migration[7.0]
   def change
     create_table :file_models do |t|
       t.string :name
+      t.string :audio_path
 
       t.timestamps
     end
