a6054e697e22173c9ecf94b97211e222c67db87d
Faizan Haider <hfaizan53@gmail.com>
2025-05-15 18:58:05 +0500

    added API

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,13 @@ Rails.application.routes.draw do
   get 'download_audio', to: 'books#download_audio'
   get '/audio', to: 'books#audio'
   get '/player_partial', to: 'books#player_partial'
+
+  # API namespace
+  namespace :api do
+    namespace :v1 do
+      post 'upload_pdf', to: 'books#upload_pdf'
+      get 'retrieve_text/:id', to: 'books#retrieve_text'
+      get 'download_audio/:id', to: 'books#download_audio'
+    end
+  end
 end
